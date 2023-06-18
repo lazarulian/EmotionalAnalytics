89f151e2a8ce15eab3a8cf4feb0b5d51a027ea6b
Apurva Shah <shahh.apurva@gmail.com>
2023-06-17 17:17:33 -0700

    Front End Hook Changes

--- front-end/pages/api/emotion.js
+++ front-end/pages/api/emotion.js
@@ -1,14 +1,10 @@
 export default async (req, res) => {
   const { text } = req.body;
 
-  const response = await fetch("http://localhost:5000/love_babu", {
+  const response = await fetch("/flask/love_babu", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      models: { language: {} },
-      raw_text: true,
-      data: text,
-    }),
+    body: text,
   });
 
   const data = await response.json();
